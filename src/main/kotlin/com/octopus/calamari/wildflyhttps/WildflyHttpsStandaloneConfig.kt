c35f88d127111478ae43ba5604ff25727360fa7d
Matthew Casperson <matthewcasperson@gmail.com>
2017-10-06 11:57:02 +1000

    Fixed up login timeouts

--- src/main/kotlin/com/octopus/calamari/wildflyhttps/WildflyHttpsStandaloneConfig.kt
+++ src/main/kotlin/com/octopus/calamari/wildflyhttps/WildflyHttpsStandaloneConfig.kt
@@ -1,10 +1,12 @@
 package com.octopus.calamari.wildflyhttps
 
 import com.octopus.calamari.exception.ExpectedException
+import com.octopus.calamari.exception.wildfly.LoginTimeoutException
 import com.octopus.calamari.utils.Constants
 import com.octopus.calamari.utils.impl.ErrorMessageBuilderImpl
 import com.octopus.calamari.utils.impl.LoggingServiceImpl
 import com.octopus.calamari.utils.impl.WildflyService
+import com.octopus.calamari.wildfly.WildflyDeploy
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -16,6 +18,14 @@ object WildflyHttpsStandaloneConfig {
         try {
             LoggingServiceImpl.configureLogging()
             WildflyHttpsStandaloneConfig.configureHttps(WildflyHttpsOptions.fromEnvironmentVars())
+        } catch (ex: LoginTimeoutException) {
+            WildflyDeploy.logger.log(Level.SEVERE, "", ex)
+            /*
+                Need to do a hard exit here because the CLI can keep things open
+                and prevent a System.exit() from working
+             */
+            LoggingServiceImpl.flushStreams()
+            Runtime.getRuntime().halt(Constants.FAILED_LOGIN_RETURN)
         } catch (ex: ExpectedException) {
             logger.log(Level.SEVERE, null, ex)
             System.exit(Constants.FAILED_DEPLOYMENT_RETURN)
